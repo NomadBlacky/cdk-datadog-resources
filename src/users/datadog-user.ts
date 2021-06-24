db7e8e20b442c913d81cf9f0254ee0584ad95b08
Daniel Maher <daniel.maher@datadoghq.com>
2021-06-24 15:15:55 +0200

    Add support for Datadog::Users::User resource

---
+++ src/users/datadog-user.ts
@@ -0,0 +1,42 @@
+import { CfnResource, Construct } from '@aws-cdk/core';
+import * as camelcaseKeys from 'camelcase-keys';
+import { DatadogCredentials } from '../common/properties';
+
+export interface DatadogIAMUserProps {
+  /** Credentials for the Datadog API */
+  readonly datadogCredentials: DatadogCredentials;
+
+  /** The role of the user */
+  readonly accessRole?: string;
+
+  /** Whether or not the user is disabled */
+  readonly disabled?: boolean;
+
+  /** User email */
+  readonly email?: string;
+
+  /** User handle (a valid email) */
+  readonly handle?: string;
+
+  /** User name */
+  readonly name?: string;
+
+  /** Whether or not the user is verified */
+  readonly verified?: boolean;
+}
+
+/**
+ * Datadog Application User 1.2.0
+ */
+export class DatadogIAMUser {
+  constructor(scope: Construct, id: string, props: DatadogIAMUserProps) {
+    const cfnProperties = camelcaseKeys(props, {
+      deep: true,
+      pascalCase: true,
+    });
+    new CfnResource(scope, id, {
+      type: 'Datadog::IAM::User',
+      properties: { ...cfnProperties },
+    });
+  }
+}
